d0c818419368d997448ebde4778481fc3ad2d4a7
Alexander <alexanderfromhell4@gmail.com>
2023-11-30 09:13:48 -0500

    feat: carusel inicial

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,6 +1,7 @@
 import style from "./Home.module.css"
 import { useLanguageStore } from '../../store/language.store'
 import { Link } from "react-router-dom"
+import Pepe from "./homeComponents/ImgCarousel/ImgCarousel"
 
 export default function Home() {
     const {languageDictionary} = useLanguageStore()
@@ -8,6 +9,7 @@ export default function Home() {
         <>
             <h1>{languageDictionary.saludo}</h1>
             <Link to="/login">Login</Link>
+            <Pepe/>
         </>
     )
 }
