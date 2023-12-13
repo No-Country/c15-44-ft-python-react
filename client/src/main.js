46be84a474317e5eb59f86627a2706fd37e51895
odran21 <jleonardoguevaram@gmail.com>
2023-12-13 15:33:10 -0500

    menu mobile desplegable

--- client/src/main.js
+++ client/src/main.js
@@ -1,26 +1,44 @@
-let menu = document.getElementById("menu")
-let botonOpen = document.getElementById("open");
-let botonClose = document.getElementById("close");
 
-botonOpen.addEventListener("click",()=>{
-	menu.style.display="flex"
-})
+// **************************************************************************************
+// **************************************************************************************
 
-botonClose.addEventListener("click",()=>{
-	menu.style.display="none";
+//----------    Menú Hamburguesa desplegable    ----------//
+const iconoMenu = document.querySelector(".icono-menu");
+const menu = document.querySelector(".cont-menu");
+const menuMobile = document.querySelector(".icono-menu");
+
+iconoMenu.addEventListener("click", function () {
+  menu.classList.toggle("active");
+  menuMobile.classList.toggle("active");
+  document.body.classList.toggle("opacity");
 });
 
 // **************************************************************************************
+// **************************************************************************************
+// let menu = document.getElementById("menu")
+// let botonOpen = document.getElementById("open");
+// let botonClose = document.getElementById("close");
+
+// botonOpen.addEventListener("click",()=>{
+// 	menu.style.display="flex"
+// })
+
+// botonClose.addEventListener("click",()=>{
+// 	menu.style.display="none";
+// });
+
+// **************************************************************************************
+// **************************************************************************************
 
 
-let activeSlide = 0;
-let slides = document.querySelectorAll('.slide');
+// let activeSlide = 0;
+// let slides = document.querySelectorAll('.slide');
 
-setInterval(() => {
-    slides[activeSlide].classList.remove('active');
-    activeSlide = (activeSlide + 1) % slides.length;
-    console.log(activeSlide)
-    slides[activeSlide].classList.add('active');
-}, 5000);
+// setInterval(() => {
+//     slides[activeSlide].classList.remove('active');
+//     activeSlide = (activeSlide + 1) % slides.length;
+//     console.log(activeSlide)
+//     slides[activeSlide].classList.add('active');
+// }, 5000);
 
-console.log(slides.length)
+// console.log(slides.length)
