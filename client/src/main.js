3f32ec970d4410359a86c659044044541452870a
odran21 <jleonardoguevaram@gmail.com>
2023-12-19 23:32:26 -0500

    agg sections

--- client/src/main.js
+++ client/src/main.js
@@ -31,14 +31,49 @@ iconoMenu.addEventListener("click", function () {
 // **************************************************************************************
 
 
-// let activeSlide = 0;
-// let slides = document.querySelectorAll('.slide');
+let activeSlide = 0;
+let slides = document.querySelectorAll('.slide');
 
-// setInterval(() => {
-//     slides[activeSlide].classList.remove('active');
-//     activeSlide = (activeSlide + 1) % slides.length;
-//     console.log(activeSlide)
-//     slides[activeSlide].classList.add('active');
-// }, 5000);
+setInterval(() => {
+    slides[activeSlide].classList.remove('activate');
+    activeSlide = (activeSlide + 1) % slides.length;
+    slides[activeSlide].classList.add('activate');
+}, 5000);
 
-// console.log(slides.length)
+// **************************************************************************************
+// **************************************************************************************
+
+// // Selecciona el elemento del contador
+// var clientesSatisfechosCounter = document.getElementById('clientes-satisfechos-counter');
+
+// // Define el valor final del contador
+// var valorFinal = 50;
+
+// // Define la duración de la animación en milisegundos
+// var duracionAnimacion = 2000; // 2000 milisegundos = 2 segundos
+
+// // Calcula la cantidad de incremento por milisegundo
+// var incrementoPorMilisegundo = valorFinal / duracionAnimacion;
+
+// // Inicializa el contador
+// var contador = 0;
+
+// // Crea una función para actualizar el contador
+// function actualizarContador() {
+//   contador += incrementoPorMilisegundo;
+  
+//   // Redondea el contador para evitar números decimales innecesarios
+//   contador = Math.round(contador);
+  
+//   // Actualiza el contenido del elemento del contador
+//   clientesSatisfechosCounter.innerHTML = contador;
+
+//   // Detén la animación cuando se alcanza el valor final
+//   if (contador >= valorFinal) {
+//     clearInterval(intervalo);
+//     clientesSatisfechosCounter.innerHTML = valorFinal;
+//   }
+// }
+
+// // Actualiza el contador cada milisegundo
+// var intervalo = setInterval(actualizarContador, 1);
